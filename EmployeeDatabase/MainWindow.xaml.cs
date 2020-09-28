4871b7b0855ff5bbfe77001027bf130b25b6d29d
Anthony Anderson <antgande@ut.utm.edu>
2020-09-28 05:59:55 -0500

    connection was a success

--- EmployeeDatabase/MainWindow.xaml.cs
+++ EmployeeDatabase/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,32 @@ namespace EmployeeDatabase
     /// </summary>
     public partial class MainWindow : Window
     {
+        OleDbConnection cn;
+        public string Text { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            cn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\EmployeeDatabase.accdb");
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select* from Assets";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            
+            while (read.Read())
+            {
+                data += read[0].ToString() + "\n";
+                Info_field.Text = data;
+
+
+            }
+            cn.Close();
+        }
+
     }
 }
